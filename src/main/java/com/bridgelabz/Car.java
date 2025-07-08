ba496589da95ce3aa49cdb618d6551aec76f56b7
GTKannan <gtkannan2311@gmail.com>
2025-07-08 16:08:00 +0530

    Constructor injection achieved

--- src/main/java/com/bridgelabz/Car.java
+++ src/main/java/com/bridgelabz/Car.java
@@ -2,7 +2,14 @@ package com.bridgelabz;
 
 public class Car {
 
+    Engine engine;
+
+    Car(Engine engine){
+        this.engine=engine;
+        System.out.println("Engine loaded inside car through constructor");
+    }
+
     public void start(){
-        System.out.println("Starting");
+        System.out.println("Starting car...");
     }
 }
