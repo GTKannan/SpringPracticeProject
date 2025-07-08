57fec08f8a5cdea672837831024e873df88e034b
GTKannan <gtkannan2311@gmail.com>
2025-07-08 15:15:15 +0530

    Create and Retrieve a Simple Bean

--- src/main/java/com/bridgelabz/App.java
+++ src/main/java/com/bridgelabz/App.java
@@ -1,13 +1,18 @@
 package com.bridgelabz;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        ApplicationContext context = new
+                ClassPathXmlApplicationContext("beans.xml");
+        Car car = (Car) context.getBean("car");
+        car.start();
     }
 }
---
+++ src/main/java/com/bridgelabz/Car.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Car {
+
+    public void start(){
+        System.out.println("Starting");
+    }
+}
