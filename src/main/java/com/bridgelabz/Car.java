7dc0ad47c6ba9f4c26f16c6ffbd275a33ff0d2b4
GTKannan <gtkannan2311@gmail.com>
2025-07-08 16:24:55 +0530

    Setter injection achieved

--- src/main/java/com/bridgelabz/Car.java
+++ src/main/java/com/bridgelabz/Car.java
@@ -1,15 +1,30 @@
 package com.bridgelabz;
 
 public class Car {
-
+    String model;
     Engine engine;
+
+    Car(){
+
+    }
 
     Car(Engine engine){
         this.engine=engine;
         System.out.println("Engine loaded inside car through constructor");
     }
 
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+        System.out.println("Engine loaded inside car through setter");
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
     public void start(){
         System.out.println("Starting car...");
+        System.out.println("car model name : "+model);
     }
+
 }
